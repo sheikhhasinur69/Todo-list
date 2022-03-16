10f7e3210e0e8c2cd796d714fc79dbf64ff7bfc1
Tushar Raihan <sheikhhasinur69@gmail.com>
2022-03-16 23:01:51 +0600

    fisrt commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+
+button.addEventListener("click", function(){
+    if(input.value.length > 0) {
+        var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+    }
+    
+})
+
+input.addEventListener("keypress", function(event){
+  
+    if(input.value.length > 0 && event.keyCode === 13) {
+        var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+    }
+    
+})
